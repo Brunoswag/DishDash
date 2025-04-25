125ea1ffbbd849fc95d533f301c09af93a067cef
jessedahlke <jesse.dahlke@ndsu.edu>
2025-04-25 08:52:20 -0500

    Added navbar (sort of)

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  navItems = [
+    { label: 'Home', icon: 'bi bi-house-door' },
+    { label: 'Search', icon: 'bi bi-search' },
+    { label: 'Explore', icon: 'bi bi-compass' },
+    { label: 'Reels', icon: 'bi bi-film' },
+    { label: 'Messages', icon: 'bi bi-chat' },
+    { label: 'Notifications', icon: 'bi bi-heart' },
+    { label: 'Create', icon: 'bi bi-plus-square' }
+  ];
 }
