0dc6fcae514f20994ff58f1ffbff4a5d3cc3d6de
Spencer lommel <spencerlommel@gmail.com>
2025-04-25 08:41:19 -0500

    recipe route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
-export const routes: Routes = [{ path: 'home', component: HomeComponent }];
+import { RecipeComponent } from './recipe/recipe.component';
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'recipe:id', component: RecipeComponent },
+];
