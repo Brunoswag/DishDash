520c1accafaeac45962c3b8a3956c605b893aba5
Brunoswag <nostrumlance@gmail.com>
2025-04-25 09:16:52 -0500

    Added basic class details for recipes

--- src/app/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-detail/recipe-detail.component.ts
@@ -7,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-detail.component.css'
 })
 export class RecipeDetailComponent {
+  recipeID = null;
+  
+  thumbnailImage: String = '';
+  optionalImages: String[] = [];
+  recipeName: String = '';
+  recipeDescription: String = '';
+  
+  introText: String = '';
+  introImage: String = '';
+  
+  closingText: String = '';
+  closingImage: String = '';
+  
+  warningText: String = '';
+  warningImage: String = '';
+  
+  recipeStepText: String[] = [];
+  recipeStepImage: String[] = [];
 
 }
