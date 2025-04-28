fe6969cdf40f9b98f4ff058286ebca2e3a6bed5a
SpencerLommel <spencerlommel@gmail.com>
2025-04-28 11:13:59 -0500

    profile picture in search bar

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,17 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { NavService } from '../navbar.service';
+import { UserService } from '../services/user.service';
+import { User } from '../models/user';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
-export class SearchComponent {
-  constructor(private navService: NavService) {}
+export class SearchComponent implements OnInit, OnDestroy {
+  user: User | null = null;
+  private userSubscription?: Subscription;
+
+  constructor(
+    private navService: NavService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit() {
+    this.userSubscription = this.userService.currentUser$.subscribe(user => {
+      this.user = user;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.userSubscription) {
+      this.userSubscription.unsubscribe();
+    }
+  }
 
   toggleNav() {
     this.navService.toggleSidebar();
+  }
+
+  getProfilePicture(): string {
+    return this.userService.getProfilePicture(this.user);
   }
 }
