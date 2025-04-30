b54c7cd830751992d8f21e604b25e132de4b21fc
jessedahlke <jesse.dahlke@ndsu.edu>
2025-04-30 16:56:23 -0500

    Added functionality to user-profile component

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { User } from '../models/user';
+import { UserService } from '../services/user.service';
+import { Subscription, Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { Timestamp } from 'firebase/firestore';
 
 @Component({
   selector: 'app-user-profile',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit, OnDestroy {
+  user: User | null = null;
+  private userSubscription?: Subscription;
+  isExpanded$!: Observable<boolean>;
 
+  rating = 5; //change once rating is implemented in recipes
+  accountCreationDate:string = '';
+  recipesCreated = 0; //change once recipes created is implemented
+  recipesSaved = 0;
+
+  formattedDate = this.user?.createdAt.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+
+  constructor(
+      private userService: UserService,
+    ) {
+    }
+
+  ngOnInit() {
+    this.userSubscription = this.userService.currentUser$.subscribe(user => {
+      this.user = user;
+    });
+    if (this.user?.createdAt instanceof Timestamp) {
+      this.user.createdAt = this.user.createdAt.toDate();
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.userSubscription) {
+      this.userSubscription.unsubscribe();
+    }
+  }
+
+  getProfilePicture(): string {
+    return this.userService.getProfilePicture(this.user);
+  }
 }
